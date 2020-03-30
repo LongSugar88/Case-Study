88719a79c8ab1e7a1d8c10ba8006aa2db0253821
longduongxd <longduongxd@gmail.com>
2020-03-30 17:19:18 +0700

    Case Study phiên bản 2 có bắn đạn và multi bullet

--- Case_Study_1.js
+++ Case_Study_1.js
@@ -2,6 +2,8 @@ let player;
 let background;
 let myScore;
 let obstacles = [];
+let bullet = [];
+let flappy_Bird = [];
 const PLAYER_WIDTH = 60;
 const PLAYER_HEIGHT = 60;
 const GAME_AREA_WIDTH = 680;
@@ -10,12 +12,7 @@ const BACKGROUND_WIDTH = 720;
 const BACKGROUND_HEIGHT = 420;
 const POSITION_X = 0;
 const POSITION_Y = GAME_AREA_HEIGHT/2 - PLAYER_HEIGHT/2;
-function startGame() {
-    player = new Component(PLAYER_WIDTH, PLAYER_HEIGHT,'https://i.imgur.com/pNbhCD4.png', POSITION_X, POSITION_Y,'image');
-    background = new Component(BACKGROUND_WIDTH, BACKGROUND_HEIGHT, 'https://i.imgur.com/bgWwpBT.jpg', 0, 0, 'background');
-    myScore = new Component('30px','Times New Roman','yellow', 280, 40,'text');
-    GameArea.start()
-}
+
 let GameArea = {
     canvas : document.createElement('canvas'),
     start : function() {
@@ -47,7 +44,7 @@ let Component = function (width, height, color, x, y, type){
         this.image.src = color;
     }
     this.update = function () {
-       let ctx = GameArea.context;
+        let ctx = GameArea.context;
         if(this.type == 'text'){
             ctx.font = 'bold ' + this.width + ' '+ this.height;
             ctx.fillStyle = color;
@@ -83,12 +80,28 @@ let Component = function (width, height, color, x, y, type){
         let rightObstacle = obj.x + obj.width;
         let dive = true;
         if( topEdge > underObstacle || underEdge < topObstacle||
-        leftEdge > rightObstacle || rightEdge < leftObstacle
+            leftEdge > rightObstacle || rightEdge < leftObstacle
         ) {
             dive = false;
         }
         return dive
     };
+    this.clear = function () {
+        ctx = GameArea.context;
+        ctx.clearRect(this.x, this.y, this.width, this.height);
+    }
+}
+function startGame() {
+    player = new Component(PLAYER_WIDTH, PLAYER_HEIGHT,'https://i.imgur.com/pNbhCD4.png', POSITION_X, POSITION_Y,'image');
+    background = new Component(BACKGROUND_WIDTH, BACKGROUND_HEIGHT, 'https://i.imgur.com/bgWwpBT.jpg', 0, 0, 'background');
+    myScore = new Component('30px','Consolas','yellow', 280, 40,'text');
+    GameArea.start()
+}
+
+let order = false;
+function imperialOrder() {
+    order = true;
+    return order;
 }
 function updateGameArea(){
     let x, height, space, maxHeight, minHeight,maxSpace, minSpace;
@@ -98,24 +111,96 @@ function updateGameArea(){
             return
         }
     }
+    for (i=0; i<bullet.length; i++){
+        for(let j=0; j<obstacles.length; j++){
+            if(bullet[i].checkDivePoint(obstacles[j])){
+                obstacles.splice(j,1);
+                bullet.splice(i,1);
+            }
+        }
+    }
+    for (i=0; i<bullet.length; i++){
+        for(let j=0; j<flappy_Bird.length; j++){
+            if(bullet[i].checkDivePoint(flappy_Bird[j])){
+                flappy_Bird.splice(j,1);
+                bullet.splice(i,1);
+            }
+        }
+    }
+    for(let j=0; j<flappy_Bird.length; j++) {
+        if (player.checkDivePoint(flappy_Bird[j])) {
+            flappy_Bird.splice(j,1);
+            GameArea.score *= 0.5;
+        }
+    }
+
     GameArea.clear();
     background.speedX = -0.5;
     background.newPosition();
-
     background.update();
     GameArea.frameNo += 1;
+    function randomNumber(max, min) {
+        let random = Math.floor(Math.random()*(max-min+1)+min);
+        return random
+    }
+    function bulletType() {
+        let image = "";
+        switch (randomNumber(6,1)) {
+            case 1:
+                image = './image/bullet_flappy_1.png' // Bullet 1
+                break;
+            case 2:
+                image = './image/bullet_flappy_2.png'  // Bullet 2
+                break;
+            case 3:
+                image = './image/bullet_flappy_3.png'  //Bullet 3
+                break;
+            case 4:
+                image = './image/bullet_flappy_4.png' //Bullet 4
+                break;
+            case 5:
+                image = './image/bullet_flappy_5.png' //Bullet 4
+                break;
+            case 6:
+                image = './image/bullet_flappy_6.png' //Bullet 4
+                break;
+        }
+        return image;
+    }
     if(GameArea.frameNo == 1 || obsAppearCondition(200)){
         x = GameArea.canvas.width;
         minHeight = 20;
         maxHeight = 200;
-        height = Math.floor(Math.random()*(maxHeight - minHeight + 1) + minHeight);
+        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
         minSpace = 80;
         maxSpace = 200;
-        space = Math.floor(Math.random()*(maxSpace - minSpace + 1) + minSpace);
+        space = Math.floor(Math.random()*(maxSpace-minSpace+1)+minSpace);
+        flappy_Width = randomNumber(70,20)
+        flappy_Y = randomNumber(height+space-flappy_Width,height+flappy_Width) - Number(flappy_Width);
         let obstacle1 = new Component(30, height, 'https://i.imgur.com/6D49450.png' , x, 0, 'image');
         let obstacle2 = new Component(30,x - height - space,'https://i.imgur.com/6D49450.png', x, height+space, 'image' );
+        let flappyBird = new Component(flappy_Width,flappy_Width,'https://i.imgur.com/pNbhCD4.png', x,flappy_Y, 'image' );
         obstacles.push(obstacle1);
         obstacles.push(obstacle2);
+        flappy_Bird.push(flappyBird);
+    }
+    // while (GameArea.score > 3){
+    if(order){
+        let bullet1 = new Component(PLAYER_WIDTH, PLAYER_HEIGHT,bulletType(), player.x + player.width, player.y, 'image')
+        bullet.push(bullet1);
+        GameArea.score -= 2;
+        order = false;
+        // }
+    }
+    for( let i=0; i<bullet.length; i++){
+        bullet[i].speedX = 3;
+        bullet[i].newPosition();
+        bullet[i].update();
+    }
+    for( let i=0; i<flappy_Bird.length; i++) {
+        flappy_Bird[i].speedX = -2;
+        flappy_Bird[i].newPosition();
+        flappy_Bird[i].update();
     }
     for(let i=0; i<obstacles.length; i++){
         obstacles[i].speedX = -1;
@@ -129,10 +214,8 @@ function updateGameArea(){
     }
     myScore.text = 'SCORE: ' + GameArea.score/2;
 
-    myScore.update()
-
+    myScore.update();
     player.newPosition();
-
     player.update();
 
 
